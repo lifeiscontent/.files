58650f842279e9e5bd7b0c8b968e33a85c7bb143
Aaron Reisman <aaron@lifeiscontent.net>
2017-05-04 17:08:41 +0700

    Add nvm to .files

--- scripts/install_packages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-$HOME/.files/scripts/run_on_data.sh brew install $HOME/.files/data/brew
-$HOME/.files/scripts/run_on_data.sh brew cask install $HOME/.files/data/cask
-$HOME/.files/scripts/run_on_data.sh yarn global add $HOME/.files/data/yarn
-$HOME/.files/scripts/run_on_data.sh apm install $HOME/.files/data/apm
--- scripts/upgrade_packages.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-$HOME/.files/scripts/run_on_data.sh brew upgrade $HOME/.files/data/brew
-$HOME/.files/scripts/run_on_data.sh brew cask upgrade $HOME/.files/data/cask
-$HOME/.files/scripts/run_on_data.sh yarn global upgrade $HOME/.files/data/yarn
-$HOME/.files/scripts/run_on_data.sh apm upgrade $HOME/.files/data/apm
